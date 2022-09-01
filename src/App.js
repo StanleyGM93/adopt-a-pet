dd88799e069b1dd30e12b93e893cc3ef8c705fcf
Stanley Mair <stanleygmair@gmail.com>
2022-09-01 18:54:46 +1200

    Fixed the path attribute on the Route component, added useParams hook for pet type

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ function App() {
     <Router>
       <Navigation />
 
-      <Route>
-        <HomePage path='/:type?'/>
+      <Route path='/:type?'>
+        <HomePage />
       </Route>
     </Router>
   );
